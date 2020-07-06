bcc98f2a2619f3359ac0f5ecc499442238670116
Maxym <maks064040@gmail.com>
2020-07-06 23:48:27 +0300

    update index

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 var bodyParser = require('body-parser')
-
+const PORT = process.env.PORT || 80;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
@@ -10,4 +10,4 @@ app.use('/', (req, res) => {
     res.send('<h1>Hello Express</h1>')
 })
 app.use('/api', require('./serverApi'))
-app.listen(4000, () => console.log("Server started"))
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
